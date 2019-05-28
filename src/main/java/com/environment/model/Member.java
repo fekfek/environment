1c6c2991c9782a9d2dc9362423432a894ac71103
fhmarian <fhmarian@yahoo.com>
2019-05-28 12:08:11 -0400

    lombok functionality used

--- src/main/java/com/environment/model/Member.java
+++ src/main/java/com/environment/model/Member.java
@@ -8,7 +8,11 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Setter @Getter
 public class Member {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -26,58 +30,6 @@ public class Member {
 	private String membershipType;
 	private String contributionRemark;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getMembershipType() {
-		return membershipType;
-	}
-	public void setMembershipType(String membershipType) {
-		this.membershipType = membershipType;
-	}
-	public String getContributionRemark() {
-		return contributionRemark;
-	}
-	public void setContributionRemark(String contributionRemark) {
-		this.contributionRemark = contributionRemark;
-	}
-
-	
-	
-	
-	
+		
 
 }
--- src/main/java/com/environment/model/Volunteer.java
+++ src/main/java/com/environment/model/Volunteer.java
@@ -5,7 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter @Setter
 public class Volunteer {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -15,46 +19,6 @@ public class Volunteer {
 	private String email;
 	private String city;
 	private String remark;
-	
-	public int getId() {
-		return Id;
-	}
-	public void setId(int id) {
-		Id = id;
-	}
-	public String getVname() {
-		return vname;
-	}
-	public void setVname(String vname) {
-		this.vname = vname;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getRemark() {
-		return remark;
-	}
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-	
-	
-
 	
 	
 
